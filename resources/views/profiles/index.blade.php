055dbb80f5c4a1d6b06d405796e6ad176023b92d
Everton Andrei Pereira <evertonandreipereira@hotmail.com>
2019-05-21 22:09:18 -0300

    criando o post para a página, erro 1364 Field 'title' doesn't have a default value (SQL: insert into  (, , , , )

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,15 +7,18 @@
             <img src="https://instagram.ffln1-1.fna.fbcdn.net/vp/ff93f0e7fbb314b6be31b2e653f517d8/5D655738/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.ffln1-1.fna.fbcdn.net" class="rounded-circle">
         </div>
         <div class="col-9 p-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-itens-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add new post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-4"><strong>153</strong> posts</div>
                 <div class="pr-4"><strong>24,1k</strong> followers</div>
                 <div class="pr-4"><strong>217</strong> following</div>
             </div>
-            <div class="pt-3 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div class="font-weight-bold"><a href="#">www.freecodecamp.org</a></div>           
+            <div class="pt-3 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div class="font-weight-bold"><a href="#">{{ $user->profile->url }}</a></div>           
         </div>
     </div>
     
